e20ee1bd45abdd1cd9db34002a4380ded1c12168
Shenghui Xue <38235049+ShenghuiXue@users.noreply.github.com>
2021-06-07 20:48:15 -0500

    Update LearnJodaTime.java

--- test-joda-time/src/main/java/edu/gatech/test_joda_time/LearnJodaTime.java
+++ test-joda-time/src/main/java/edu/gatech/test_joda_time/LearnJodaTime.java
@@ -1,5 +1,64 @@
 package edu.gatech.test_joda_time;
 
-public class LearnJodaTime {
+import java.util.Locale;
+import org.joda.time.format.DateTimeFormat;
 
+public class App 
+{
+    public static void main( String[] args )
+    {    	
+    	printDateTimeFormat (Locale.UK);
+    	printDateTimeFormat (Locale.FRENCH);
+    	printDateTimeFormat (Locale.US);
+    	printDateTimeFormat (Locale.CANADA);
+    }
+    
+    /**
+     * Print out the date time format for the input locale
+     * @param locale The {@link Locale}
+     */
+    private static void printDateTimeFormat (Locale locale) {
+    	String format = DateTimeFormat.patternForStyle("S-", locale);
+    	
+    	System.out.println("****************************************************** ");
+    	System.out.println("locale: " + locale );
+    	System.out.println( "format: " + format );
+    	System.out.println("java version: " + System.getProperty("java.version"));
+    }
+    
+   /* OUTPUT:       
+    ****************************************************** 
+		locale: en_GB
+		format: dd/MM/y
+		java version: 11
+		****************************************************** 
+		locale: fr
+		format: dd/MM/y
+		java version: 11
+		****************************************************** 
+		locale: en_US
+		format: M/d/yy
+		java version: 11
+		****************************************************** 
+		locale: en_CA
+		format: y-MM-dd
+		java version: 11
+		
+		****************************************************** 
+		locale: en_GB
+		format: dd/MM/yy
+		java version: 1.8.0_221
+		****************************************************** 
+		locale: fr
+		format: dd/MM/yy
+		java version: 1.8.0_221
+		****************************************************** 
+		locale: en_US
+		format: M/d/yy
+		java version: 1.8.0_221
+		****************************************************** 
+		locale: en_CA
+		format: dd/MM/yy
+		java version: 1.8.0_221
+   */
 }
